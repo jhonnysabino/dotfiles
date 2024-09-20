c0c7f96a6020c44dc1219d3583301b0cc6183af6
Jhonny <jhonny.sabino@bemobi.com.br>
2024-09-20 01:08:26 -0300

    feat(nvim): add support to kotlin on lsp, formatter and linter

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -19,14 +19,30 @@ return {
 		config = function()
 			local lspconfig = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local util = require("lspconfig.util")
+
+			vim.keymap.set("n", "<leader>gf", function()
+				vim.lsp.buf.format({ timeout_ms = 5000 })
+			end, { desc = "Format code" })
 
 			lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
+				capabilities = capabilities,
+			})
 			lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-
+				capabilities = capabilities,
+			})
+			lspconfig.kotlin_language_server.setup({
+				capabilities = capabilities,
+				cmd = { vim.fn.stdpath("data") .. "/mason/bin/kotlin-language-server" },
+				cmd_env = {
+					JAVA_HOME = "/home/jhonny/.sdkman/candidates/java/11.0.11-open",
+					PATH = vim.fn.stdpath("data")
+						.. "/mason/bin:"
+						.. "/home/jhonny/.sdkman/candidates/java/11.0.11-open/bin:"
+						.. vim.env.PATH,
+				},
+				root_dir = util.root_pattern("settings.gradle", "build.gradle", ".git"),
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,17 +1,21 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.biome,
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.biome,
+                null_ls.builtins.formatting.ktlint,
+                null_ls.builtins.diagnostics.ktlint,
+            },
+            timeout_ms = 30000,
+            timeout = 30,
+        })
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
 }
